82d57fda5ce30cc197d4708f4599f3e3f887f70b
Jakub Kolodziejczyk <jakub.kolodziejczyk@mail.com>
2017-04-17 21:51:54 +0900

    Added net.conversions.get_images_batch_patches_matrix()

--- net/conversions.py
+++ net/conversions.py
@@ -6,6 +6,14 @@ import numpy as np
 
 
 def get_image_patches_matrix(image, kernel_shape):
+    """
+    Convert a 3D image into a matrix of patches such that each row is a single patch
+     that can be convolved with a kernel of provided shape
+    :param image: 3D numpy array
+    :param kernel_shape: kernel shape, 3rd dimension must be the same as that of image
+    :return: 2D numpy array, rows represent image patches. Image patches are scanned in horizontal order
+    with a step of 1
+    """
 
     vertical_steps = image.shape[0] - kernel_shape[0] + 1
     horizontal_steps = image.shape[1] - kernel_shape[1] + 1
@@ -24,3 +32,39 @@ def get_image_patches_matrix(image, kernel_shape):
             image_matrix[index] = image_patch.flatten()
 
     return image_matrix
+
+
+def get_images_batch_patches_matrix(images, kernel_shape):
+    """
+    Convert a batch of 3D images into a single matrix of patches such that each row is a single patch
+     that can be convolved with a kernel of provided shape
+    :param images: 4D numpy array
+    :param kernel_shape: kernel shape, 3rd dimension must be the same as last dimension of images array
+    :return: 2D numpy array, rows represent image patches. Image patches are scanned in horizontal order
+    with a step of 1. After one image ends, second image is scanned, and so on.
+    """
+
+    image_steps = images.shape[0]
+    vertical_steps = images.shape[1] - kernel_shape[0] + 1
+    horizontal_steps = images.shape[2] - kernel_shape[1] + 1
+
+    shape = (image_steps * vertical_steps * horizontal_steps, np.product(kernel_shape))
+    matrix = np.zeros(shape)
+
+    for image_index in range(images.shape[0]):
+
+        for row_index in range(images.shape[1] - kernel_shape[0] + 1):
+
+            for column_index in range(images.shape[2] - kernel_shape[1] + 1):
+
+                patch_index = (image_index, slice(row_index, row_index + kernel_shape[0]),
+                               slice(column_index, column_index + kernel_shape[1]))
+
+                image_patch = images[patch_index]
+
+                matrix_index = (image_index * vertical_steps * horizontal_steps) + \
+                               (row_index * horizontal_steps) + column_index
+
+                matrix[matrix_index] = image_patch.flatten()
+
+    return matrix
--- tests/net/test_conversions.py
+++ tests/net/test_conversions.py
@@ -121,3 +121,118 @@ def test_get_image_patches_matrix_4x4x1_image():
     actual = net.conversions.get_image_patches_matrix(image, kernel_shape)
 
     assert np.all(expected == actual)
+
+
+def test_get_images_batch_patches_matrix_single_2x2x1_image():
+
+    images = np.arange(4).reshape(1, 2, 2, 1)
+    kernel_shape = (2, 2, 1)
+
+    expected = np.array([0, 1, 2, 3]).reshape(1, 4)
+    actual = net.conversions.get_images_batch_patches_matrix(images, kernel_shape)
+
+    assert np.all(expected == actual)
+
+
+def test_get_images_batch_patches_matrix_two_2x2x1_images():
+
+    first_image = np.arange(4).reshape(2, 2, 1)
+    second_image = np.arange(10, 14).reshape(2, 2, 1)
+
+    images = np.array([first_image, second_image])
+    kernel_shape = (2, 2, 1)
+
+    expected = np.array([
+        [0, 1, 2, 3],
+        [10, 11, 12, 13]
+    ])
+
+    actual = net.conversions.get_images_batch_patches_matrix(images, kernel_shape)
+
+    assert np.all(expected == actual)
+
+
+def test_get_images_batch_patches_matrix_two_2x2x2_images():
+
+    first_image = np.arange(8).reshape(2, 2, 2)
+    second_image = np.arange(10, 18).reshape(2, 2, 2)
+
+    images = np.array([first_image, second_image])
+    kernel_shape = (2, 2, 2)
+
+    expected = np.array([
+        [0, 1, 2, 3, 4, 5, 6, 7],
+        [10, 11, 12, 13, 14, 15, 16, 17],
+    ])
+
+    actual = net.conversions.get_images_batch_patches_matrix(images, kernel_shape)
+
+    assert np.all(expected == actual)
+
+
+def test_get_images_batch_patches_matrix_two_3x3x2_images_2x2x2_kernel():
+
+    first_image = np.arange(18).reshape(3, 3, 2)
+    second_image = np.arange(20, 38).reshape(3, 3, 2)
+
+    images = np.array([first_image, second_image])
+    kernel_shape = (2, 2, 2)
+
+    expected = np.array([
+        [0, 1, 2, 3, 6, 7, 8, 9],
+        [2, 3, 4, 5, 8, 9, 10, 11],
+        [6, 7, 8, 9, 12, 13, 14, 15],
+        [8, 9, 10, 11, 14, 15, 16, 17],
+        [20, 21, 22, 23, 26, 27, 28, 29],
+        [22, 23, 24, 25, 28, 29, 30, 31],
+        [26, 27, 28, 29, 32, 33, 34, 35],
+        [28, 29, 30, 31, 34, 35, 36, 37]
+    ])
+
+    actual = net.conversions.get_images_batch_patches_matrix(images, kernel_shape)
+
+    assert np.all(expected == actual)
+
+
+def test_get_images_batch_patches_matrix_two_4x2x1_images_2x2x1_kernel():
+
+    first_image = np.arange(8).reshape(4, 2, 1)
+    second_image = np.arange(10, 18).reshape(4, 2, 1)
+
+    images = np.array([first_image, second_image])
+    kernel_shape = (2, 2, 1)
+
+    expected = np.array([
+        [0, 1, 2, 3],
+        [2, 3, 4, 5],
+        [4, 5, 6, 7],
+        [10, 11, 12, 13],
+        [12, 13, 14, 15],
+        [14, 15, 16, 17]
+    ])
+
+    actual = net.conversions.get_images_batch_patches_matrix(images, kernel_shape)
+
+    assert np.all(expected == actual)
+
+
+def test_get_images_batch_patches_matrix_two_2x4x1_images_2x2x1_kernel():
+
+    first_image = np.arange(8).reshape(2, 4, 1)
+    second_image = np.arange(10, 18).reshape(2, 4, 1)
+
+    images = np.array([first_image, second_image])
+    kernel_shape = (2, 2, 1)
+
+    expected = np.array([
+        [0, 1, 4, 5],
+        [1, 2, 5, 6],
+        [2, 3, 6, 7],
+        [10, 11, 14, 15],
+        [11, 12, 15, 16],
+        [12, 13, 16, 17]
+    ])
+
+    actual = net.conversions.get_images_batch_patches_matrix(images, kernel_shape)
+
+    assert np.all(expected == actual)
