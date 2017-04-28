72928a234522392222203d616cb3b15750e001e7
Jakub Kolodziejczyk <jakub.kolodziejczyk@mail.com>
2017-04-28 20:27:00 +0900

    Refactored examples script

--- examples/mnist.py
+++ examples/mnist.py
@@ -1,15 +1,12 @@
 """
-A simple MNIST network with a single hidden layer
+A simple MNIST network with two hidden layers
 """
-import time
-
-import os
 
 import tqdm
 import numpy as np
-import keras.datasets.mnist
-import keras.utils.np_utils
 import sklearn.utils
+import sklearn.datasets
+import sklearn.preprocessing
 
 import net.layers
 import net.models
@@ -17,29 +14,39 @@ import net.models
 
 def main():
 
-    path = os.path.abspath("../../data/mnist/mnist.pkl.gz")
-    (X_train, y_train), (X_test, y_test) = keras.datasets.mnist.load_data(path)
+    print("Loading data...")
+    mnist = sklearn.datasets.fetch_mldata('MNIST original')
+
+    X_train, y_train = mnist.data[:60000], mnist.target[:60000].reshape(-1, 1)
+    X_test, y_test = mnist.data[60000:], mnist.target[60000:].reshape(-1, 1)
+
+    X_train = X_train.reshape(-1, 28, 28)
+    X_test = X_test.reshape(-1, 28, 28)
+
+    encoder = sklearn.preprocessing.OneHotEncoder(sparse=False).fit(y_train)
+
+    y_train = encoder.transform(y_train)
+    y_test = encoder.transform(y_test)
 
     # Stack images so they are 3D, scale them to <0, 1> range
     X_train = np.stack([X_train], axis=-1) / 255
     X_test = np.stack([X_test], axis=-1) / 255
 
-    y_train = keras.utils.np_utils.to_categorical(y_train)
-    y_test = keras.utils.np_utils.to_categorical(y_test)
-
     layers = [
         net.layers.Input(sample_shape=(28, 28, 1)),
-        net.layers.Convolution2D(nb_filter=10, nb_row=28, nb_col=28),
+        net.layers.Convolution2D(nb_filter=20, nb_row=14, nb_col=14),
+        net.layers.Convolution2D(nb_filter=10, nb_row=15, nb_col=15),
         net.layers.Flatten(),
         net.layers.Softmax()
     ]
 
     model = net.models.Model(layers)
 
-    batch_size = 32
+    batch_size = 128
     epochs = 20
 
-    print("Accuracy: {}".format(model.get_accuracy(X_test, y_test)))
+    X_test, y_test = sklearn.utils.shuffle(X_test, y_test)
+    print("Initial accuracy: {}".format(model.get_accuracy(X_test, y_test)))
 
     for epoch in range(epochs):
 
@@ -47,8 +54,7 @@ def main():
 
         X_train, y_train = sklearn.utils.shuffle(X_train, y_train)
 
-        # batches_count = len(X_train) // batch_size
-        batches_count = 100
+        batches_count = len(X_train) // batch_size
 
         for batch_index in tqdm.tqdm(range(batches_count)):
 
